f795d604e7c94be7de162ca30db0c5c73a4164cf
Muhammad Raihan Wahyu Yuwono <raihanyuwono@hotmail.com>
2025-02-12 09:23:52 +0700

    fix bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
-import Image from "next/image";
 import "./styles.css";
 
 import LinkTree from "./components/LinkTree";
-import { getAsset } from "./utils/get";
-
-const bgImage = {
-  src: "bg",
-  alt: "Dark Forest",
-};
 
 export default function Home() {
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <Image
-        className="bg-image"
-        src={getAsset("image", bgImage.src, "webp")}
-        alt={bgImage.alt}
-        loading="lazy"
-        fill
-      />
+    <div className="home">
       <LinkTree />
     </div>
   );
